f0637db59f56ad312b3e93c2e5a2e02606371a4e
Amruta Ghate <amrutaghate@Amrutas-MacBook-Pro.local>
2020-04-03 13:50:22 -0400

    all

--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $("#japanpic").hide();
-	$("#aboutmecont").css("padding-bottom", "50px");
-    $("#pic_button").on("click", function(){
-        $("#japanpic").toggle();
-		$("#aboutmecont").css("padding-bottom", "300px");		
-    });
-});
